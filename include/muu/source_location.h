ba1975e2a57a8b27527854864d7551382d418d8f
Mark Gillard <marzer_@hotmail.com>
2022-10-28 15:46:41 +0300

    added `source_location::file_name_tail()`

--- include/muu/source_location.h
+++ include/muu/source_location.h
@@ -19,6 +19,28 @@
 #include "fwd.h"
 #include "impl/header_start.h"
 
+/// \cond
+namespace muu::impl
+{
+	MUU_PURE_GETTER
+	MUU_CONSTEVAL
+	const char* sloc_file_name_tail(const char* const file_path) noexcept
+	{
+		const char* end = file_path;
+		while (*end)
+			end++;
+		const auto len = static_cast<size_t>(end - file_path);
+
+		const char* slash = {};
+		for (auto i = len; i-- > 0u && !slash;)
+			if (file_path[i] == '\\' || file_path[i] == '/')
+				slash = file_path + i;
+
+		return slash ? slash + 1u : file_path;
+	}
+}
+/// \endcond
+
 namespace muu
 {
 	/// \brief	A backport of C++20's std::source_location.
@@ -30,8 +52,9 @@ namespace muu
 	  private:
 		uint_least32_t line_{};
 		uint_least32_t column_{};
-		const char* file_	  = "";
-		const char* function_ = "";
+		const char* file_	   = "";
+		const char* function_  = "";
+		const char* file_tail_ = "";
 
 	  public:
 		MUU_NODISCARD
@@ -42,13 +65,15 @@ namespace muu
 									   const char* const function  = __builtin_FUNCTION()) noexcept
 		{
 			source_location loc{};
-			loc.line_	  = line;
-			loc.column_	  = column;
-			loc.file_	  = file;
-			loc.function_ = function;
+			loc.line_	   = line;
+			loc.column_	   = column;
+			loc.file_	   = file;
+			loc.function_  = function;
+			loc.file_tail_ = impl::sloc_file_name_tail(file);
 			return loc;
 		}
 
+		/// \brief Default destructor.
 		MUU_NODISCARD_CTOR
 		constexpr source_location() noexcept = default;
 
@@ -60,13 +85,14 @@ namespace muu
 		}
 
 		/// \brief The calling column.
+		/// \availability This will always be `0` on some compilers.
 		MUU_NODISCARD
 		constexpr uint_least32_t column() const noexcept
 		{
 			return column_;
 		}
 
-		/// \brief The calling file name.
+		/// \brief The calling file path.
 		MUU_NODISCARD
 		constexpr const char* file_name() const noexcept
 		{
@@ -78,6 +104,15 @@ namespace muu
 		constexpr const char* function_name() const noexcept
 		{
 			return function_;
+		}
+
+		/// \brief The filename-only 'tail' of the calling file path.
+		///
+		/// \attention	This method is a non-standard extension to std::source_location.
+		MUU_NODISCARD
+		constexpr const char* file_name_tail() const noexcept
+		{
+			return file_tail_;
 		}
 	};
 }
