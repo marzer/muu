b6418794c75eb8c6700499af5f494229d84257e0
William Roy <wroy@proton.me>
2022-11-11 07:56:42 -0500

    Fix missing ';'

--- include/muu/integer_aliases.h
+++ include/muu/integer_aliases.h
@@ -13,7 +13,7 @@ namespace muu
 	inline namespace integer_aliases
 	{
 		using i8  = int8_t;
-		using i16 = int16_t
+		using i16 = int16_t;
 		using i32 = int32_t;
 		using i64 = int64_t;
 		using u8  = uint8_t;
